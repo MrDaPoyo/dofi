e602c42d1609eab2971307320a7c56378fe58841
MrDaPoyo <wiiuforever25@gmail.com>
2025-06-22 21:22:24 +0200

    full arrays

--- balena/builtins.go
+++ balena/builtins.go
@@ -8,12 +8,69 @@ var builtins = map[string]*Builtin{
 					len(args))
 			}
 			switch arg := args[0].(type) {
+			case *Array:
+				return &ObjectInteger{Value: int64(len(arg.Elements))}
 			case *String:
 				return &ObjectInteger{Value: int64(len(arg.Value))}
 			default:
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
+		},
+	},
+	"first": &Builtin{
+		Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			if args[0].Type() != ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s",
+					args[0].Type())
+			}
+			arr := args[0].(*Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return EVAL_NULL
+		},
+	},
+	"last": &Builtin{
+		Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			if args[0].Type() != ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s",
+					args[0].Type())
+			}
+			arr := args[0].(*Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+			return EVAL_NULL
+		},
+	},
+	"rest": &Builtin{
+		Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			if args[0].Type() != ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s",
+					args[0].Type())
+			}
+			arr := args[0].(*Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &Array{Elements: newElements}
+			}
+			return EVAL_NULL
 		},
 	},
 }
