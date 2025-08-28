0401c537c8f897804f1fcb97337d38f17df7209a
MrDaPoyo <wiiuforever25@gmail.com>
2025-08-28 12:53:17 +0200

    correct line rendering

--- editors/text_editor.c
+++ editors/text_editor.c
@@ -11,7 +11,7 @@ void RenderLine(const char *str, int index)
     int navY = GetNavHeight();
     int fontSize = 5;
 
-    int y = index * fontSize + GAP * index + navY;
+    int y = index * fontSize + GAP * (index + 1) + navY;
 
     char y_str[32];
     sprintf(y_str, "%d", index);
@@ -24,21 +24,18 @@ void RenderTextEditor(void)
     RenderLine("hey there babe 0", 0);
     RenderLine("hey there babe 1", 1);
     RenderLine("hey there babe 2", 2);
-    RenderLine("hey there babe 2", 3);
-    RenderLine("hey there babe 2", 4);
-    RenderLine("hey there babe 2", 5);
-    RenderLine("hey there babe 2", 6);
-    RenderLine("hey there babe 2", 7);
-    RenderLine("hey there babe 2", 8);
-    RenderLine("hey there babe 2", 9);
-    RenderLine("hey there babe 2", 10);
-    RenderLine("hey there babe 2", 11);
-    RenderLine("hey there babe 2", 12);
-    RenderLine("hey there babe 2", 13);
-    RenderLine("hey there babe 14", 14);
-    RenderLine("hey there babe 15", 15);
-    RenderLine("hey there babe 16", 16);
-    RenderLine("hey there babe 17", 17);
-    RenderLine("hey there babe 18", 18);
-    RenderLine("hey there babe 19", 19);
+    RenderLine("hey there babe 3", 3);
+    RenderLine("hey there babe 4", 4);
+    RenderLine("hey there babe 5", 5);
+    RenderLine("hey there babe 6", 6);
+    RenderLine("hey there babe 7", 7);
+    RenderLine("hey there babe 8", 8);
+    RenderLine("hey there babe 9", 9);
+    RenderLine("hey there babe 10", 10);
+    RenderLine("hey there babe 11", 11);
+    RenderLine("hey there babe 12", 12);
+    RenderLine("hey there babe 13", 13);
 }
+
+// there is a 14 line limit. I still gotta implement scrolling and cursors and actual text, etc etc
+
