89441cc745e886e65f27bc4491b36fa6a041cbc3
MrDaPoyo <wiiuforever25@gmail.com>
2025-09-18 20:23:31 +0200

    render sprite

--- src/editors/sprite_editor.c
+++ src/editors/sprite_editor.c
@@ -2,28 +2,13 @@
 #include "editors.h"
 #include "../display.h"
 #include "sprite_editor.h"
-
 #include "../colors.h"
 
 struct Spritesheet spritesheet;
 struct Sprite CurrentSprite;
-
-void SetSpritePixel(size_t spriteId, size_t x, size_t y, Color color) {
-    spritesheet.sprites[spriteId]->data[x][y] = color;
-}
-
-#define DRAWING_TILES_X 2
-#define DRAWING_TILES_Y 2
-
 bool spriteEditorInitialized = false;
 
-/*
-void renderImageEditor(struct Sprite sprite) {
-    DrawRectangle(0, 0, EDITOR_WIDTH, EDITOR_HEIGHT, BLACK);
-}
-*/
-
-struct Spritesheet newSprite(struct Spritesheet spritesheet, size_t id) {
+static struct Sprite InitSprite(void) {
     struct Sprite s = {
         .width = MAX_SPRITE_WIDTH,
         .height = MAX_SPRITE_HEIGHT,
@@ -31,9 +16,55 @@ struct Spritesheet newSprite(struct Spritesheet spritesheet, size_t id) {
 
     for (size_t y = 0; y < MAX_SPRITE_HEIGHT; y++) {
         for (size_t x = 0; x < MAX_SPRITE_WIDTH; x++) {
-            s.data[y][x] = (Color){0, 0, 0, 255};
+            s.data[y][x] = (Color){0, 0, 255, 255};
         }
     }
+    return s;
+}
+
+void SetSpritePixel(size_t spriteId, size_t x, size_t y, Color color) {
+    size_t row = spriteId / TOTAL_MAX_SPRITES_SQRT;
+    size_t col = spriteId % TOTAL_MAX_SPRITES_SQRT;
+    spritesheet.sprites[row][col].data[y][x] = color;
+}
+
+#define DRAWING_EDITOR_X 2
+#define DRAWING_EDITOR_Y (2 + NAVBAR_HEIGHT)
+
+void renderImageEditor(struct Sprite sprite) {
+    DrawRectangle(
+        DRAWING_EDITOR_X,
+        DRAWING_EDITOR_Y,
+        sprite.width * EDITOR_SCALE,
+        sprite.height * EDITOR_SCALE,
+        BLACK
+    );
+
+    for (size_t y = 0; y < sprite.height; y++) {
+        for (size_t x = 0; x < sprite.width; x++) {
+            Color c = sprite.data[y][x];
+            DrawRectangle(
+                DRAWING_EDITOR_X + (int)(x * EDITOR_SCALE),
+                DRAWING_EDITOR_Y + (int)(y * EDITOR_SCALE),
+                EDITOR_SCALE,
+                EDITOR_SCALE,
+                c
+            );
+        }
+    }
+
+    DrawRectangleLines(
+        DRAWING_EDITOR_X,
+        DRAWING_EDITOR_Y,
+        sprite.width * EDITOR_SCALE,
+        sprite.height * EDITOR_SCALE,
+        systemPalette[2]
+    );
+}
+
+
+struct Spritesheet newSprite(struct Spritesheet spritesheet, size_t id) {
+    struct Sprite s = InitSprite();
 
     size_t row = id / TOTAL_MAX_SPRITES_SQRT;
     size_t col = id % TOTAL_MAX_SPRITES_SQRT;
@@ -43,9 +74,15 @@ struct Spritesheet newSprite(struct Spritesheet spritesheet, size_t id) {
     return spritesheet;
 }
 
-void RenderSpriteEditor(void)
-{
+void RenderSpriteEditor(void) {
     if (!displayNavbar) {
         ShowNavbar();
     }
+
+    if (!spriteEditorInitialized) {
+        CurrentSprite = InitSprite();
+        spriteEditorInitialized = true;
+    }
+
+    renderImageEditor(CurrentSprite);
 }
--- src/fonts.h
+++ src/fonts.h
@@ -8,7 +8,6 @@
 
 #include "raylib.h"
 #include <stddef.h>
-#include <stdlib.h>
 
 extern Font GeneralFont;
 
